677e3d7cf8b9fdc263b8da0c313c5978f14f6035
Jasik <rogozhkin87@gmail.com>
2020-12-03 22:47:32 +0900

    Created new project

---
+++ Gurunavi/Gurunavi/GurunaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  GurunaviApp.swift
+//  Gurunavi
+//
+//  Created by Vladimir Rogozhkin on 2020/12/03.
+//
+
+import SwiftUI
+
+@main
+struct GurunaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
