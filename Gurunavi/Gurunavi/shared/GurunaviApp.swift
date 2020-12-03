39320d3ec558b17021e2d4d7b1f1152f4a623c5d
Jasik <rogozhkin87@gmail.com>
2020-12-04 00:15:37 +0900

    Updated with swiftFormat

--- Gurunavi/Gurunavi/shared/GurunaviApp.swift
+++ Gurunavi/Gurunavi/shared/GurunaviApp.swift
@@ -7,8 +7,16 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        Logger.setup()
+        return true
+    }
+}
+
 @main
 struct GurunaviApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
