c714611d20c893ca01d4f0e428e5b0af6cb8580e
Jasik <rogozhkin87@gmail.com>
2020-12-05 19:38:00 +0900

    Created TopMainView

--- Gurunavi/Gurunavi/Screens/Main/TopMainView.swift
+++ Gurunavi/Gurunavi/Screens/Main/TopMainView.swift
@@ -9,21 +9,49 @@ import SwiftUI
 
 struct TopMainView: View {
     @State var searchText: String = ""
+    @State private var distance: Int = 0
+    private let distances = ["Not selected", "100m", "300m", "500m", "1km"]
 
     var body: some View {
-        VStack {
-            HStack {
+        VStack(spacing: 20) {
+            HStack(spacing: 10) {
                 TextField("Search restoran", text: $searchText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .textFieldStyle(BottomLineTextFieldStyle())
                 Button(action: {
                     Logger.info("Search Button")
                 }, label: {
                     Image(systemName: "magnifyingglass")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 30, height: 30)
+                        .frame(width: 28, height: 28)
                         .foregroundColor(.main_red)
                 })
+            }
+            HStack {
+                Button(action: {
+                    Logger.info("current geo position")
+                }, label: {
+                    HStack {
+                        Text("Current geo")
+                            .foregroundColor(.main_red)
+                        Image(systemName: "location")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 28, height: 28)
+                            .foregroundColor(.main_red)
+                    }
+                })
+                
+                Spacer()
+                
+                Menu("Distance: \(distances[distance])") {
+                    Picker("Chose distance",selection: $distance) {
+                        ForEach(0 ..< distances.count, id: \.self) {
+                            Text(distances[$0])
+                        }
+                    }
+                }
+                .foregroundColor(.main_red)
             }
         }
         .padding(.horizontal, 20)
