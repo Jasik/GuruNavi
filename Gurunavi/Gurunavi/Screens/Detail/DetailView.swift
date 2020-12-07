a273f7751354c6c4f06c69aaece3c19ab0ec8365
Jasik <rogozhkin87@gmail.com>
2020-12-07 22:34:25 +0900

    Added UI for DetailView

--- Gurunavi/Gurunavi/Screens/Detail/DetailView.swift
+++ Gurunavi/Gurunavi/Screens/Detail/DetailView.swift
@@ -11,7 +11,7 @@ struct DetailView: View {
     @State var index: Int = 0
     let images = ["1-1", "1-2", "1-3", "1-4"]
     var body: some View {
-        VStack {
+        VStack(spacing: 15) {
             PagingView(index: $index.animation(), maxIndex: images.count - 1) {
                 ForEach(images, id: \.self) { imageName in
                     Image(imageName)
@@ -20,7 +20,41 @@ struct DetailView: View {
                 }
             }
             .aspectRatio(4 / 3, contentMode: .fit)
-            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .edgesIgnoringSafeArea(.top)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Name of Restoran")
+                        .font(.title)
+                        .foregroundColor(.primary)
+                    Text("Name of translate")
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            HStack {
+                Text("Distance: ")
+                Text("500m")
+                
+                Spacer()
+                
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                Text("3.0/5.0")
+            }
+            .padding(.horizontal, 20)
+            
+            HStack {
+                Text("Address: Japan Tokyo Gotanda 2-13-20 1B")
+                Spacer()
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
         }
     }
 }
