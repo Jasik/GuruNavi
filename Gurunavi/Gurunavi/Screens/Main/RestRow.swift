53ff9b7ab8b9a751b97500b0b50c5f7770474150
Jasik <rogozhkin87@gmail.com>
2020-12-14 21:03:22 +0900

    Created RestRow

---
+++ Gurunavi/Gurunavi/Screens/Main/RestRow.swift
@@ -0,0 +1,20 @@
+//
+//  RestRow.swift
+//  Gurunavi
+//
+//  Created by Vladimir Rogozhkin on 2020/12/07.
+//
+
+import SwiftUI
+
+struct RestRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestRow_Previews: PreviewProvider {
+    static var previews: some View {
+        RestRow()
+    }
+}
